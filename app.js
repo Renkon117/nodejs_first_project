716150be7fc271794bc458758a84bd19329b6280
okada001 <okada001@rangers.uwp.edu>
2019-06-05 22:19:50 -0500

    "created POST request"

--- app.js
+++ app.js
@@ -5,18 +5,50 @@ const app = express ()
 const morgan = require ("morgan")
 const mysql = require ("mysql")
 require('dotenv').config();
+const bodyParser = require("body-parser")
 
-app.use(morgan('combined'))
+
+app.use(bodyParser.urlencoded({extended:false}))
+
+app.use(express.static("./public"))
+
+app.use(morgan('short'))
+
+app.post("/user_create", (req, res) =>{
+  console.log("Trying to create a new user...")
+  console.log("How do we get form data?")
+
+  console.log("First name: " + req.body.create_first_name)
+  const firstName = req.body.create_first_name
+  const lastName = req.body.create_last_name
+
+
+  const queryString = "INSERT INTO users (first_name, last_name) VALUES (?,?)"
+  getConnection().query(queryString, [firstName, lastName], (err, results, fields) => {
+    if (err){
+      console.log("Failed to insert new user" +err)
+      res.sendStatus(500)
+      return
+    }
+
+    console.log("Inserted a new user with id: ", results.insertedId);
+    res.end()
+  })
+  res.end()
+})
+
+function getConnection(){
+  return mysql.createConnection({host:process.env.DB_HOST,
+  user:process.env.DB_USER,
+  password:process.env.DB_PASS,
+  database:process.env.DB_DATABASE
+  })
+}
 
 app.get("/user/:id", (req, res) =>{
   console.log("Fetching user with id: " + req.params.id)
 
-  const connection = mysql.createConnection({
-    host:process.env.DB_HOST,
-    user:process.env.DB_USER,
-    password:process.env.DB_PASS,
-    database:process.env.DB_DATABASE
-  })
+  const connection = getConnection()
 
   const userID = req.params.id
   const queryString = "SELECT * FROM users WHERE id = ?"
@@ -38,7 +70,6 @@ app.get("/user/:id", (req, res) =>{
     res.json(users)
   })
 
-  //res.end()
 });
 
 app.get("/",(req, res) => {
@@ -47,9 +78,24 @@ app.get("/",(req, res) => {
 })
 
 app.get("/users", (req, res) => {
-  const user1 = {firstname: "Stephen", lastname: "Curry"}
-  const user2 = {firstname: "Kevin", lastname: "Durant"}
-  res.json({user1, user2})
+  const connection = mysql.createConnection({
+    host:process.env.DB_HOST,
+    user:process.env.DB_USER,
+    password:process.env.DB_PASS,
+    database:process.env.DB_DATABASE
+  })
+
+  const queryString = "SELECT * FROM users"
+  connection.query(queryString, (err, rows, fields) =>{
+
+    if (err) {
+      console.log("Failed to query for users: " + err)
+      res.sendStatus(500)
+      return
+    }
+
+    res.json(rows)
+  })
 })
 
 
